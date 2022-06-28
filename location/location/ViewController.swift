c4854a53201c51f8ab2cae422abe79d6451f75a4
Lalith Sasubilli <68266855+lsasubilli@users.noreply.github.com>
2022-06-28 22:06:05 +0530

    location of a user
    using swift and objective C

---
+++ location/location/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  location
+//
+//  Created by 2022 on 6/28/22.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    let centerMapButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "location-arrow-flat").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCenterLocation), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @objc func handleCenterLocation() {
+        centerMapOnUserLocation()
+        centerMapButton.alpha = 0
+    }
+    func centerMapOnUserLocation() {
+        guard let coordinate = manager.location?.coordinate else { return }
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest //battery
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    func render(_ location: CLLocation){
+        
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
+}
+
